e35cf31abe987b53e1afb633cc40d949f1275a73
Liam Griffiths <liamgriffiths@gmail.com>
2024-01-04 18:09:16 -0500

    rm bakllava

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,6 @@ export * as Adapter from "substrate/Adapter";
 export { Mistral } from "substrate/Mistral";
 export { Jina } from "substrate/Jina";
 export { StableDiffusion } from "substrate/StableDiffusion";
-export { Bakllava } from "substrate/Bakllava";
 
 export { Substrate };
 export default Substrate;
