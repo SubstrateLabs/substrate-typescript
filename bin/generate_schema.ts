3ac2b74b682ed54e9a04d1e4bce3a6057752e620
rishabh <rishabh@substrate.run>
2024-02-26 19:45:22 -0500

    add comments to generate script

--- bin/generate_schema.ts
+++ bin/generate_schema.ts
@@ -1,5 +1,7 @@
 #!/usr/bin/env -S npm run ts-node --transpileOnly
 
+/* Note: this script is used in the monorepo via Github Action: .github/workflows/validate-model-gen.yml
+   Please be careful when changing this. */
 import { execSync } from "node:child_process";
 
 const DIR = "src/API";
@@ -8,6 +10,7 @@ const ok = (message: string) => console.log("\x1b[32m✓\x1b[0m", message);
 
 execSync(`mkdir -p ${DIR}`);
 
+// Used by the Github Action mentioned above
 if (process.argv[2] && process.argv[2] === '--local') {
   console.log('Skipping OpenAPI download');
 } else {
