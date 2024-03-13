dc2b96f460c9f4f90e3d77012e49e905e65f3e94
benzguo <ben@substrate.run>
2024-03-13 11:33:28 -0400

    update example

--- examples/basic.ts
+++ examples/basic.ts
@@ -4,26 +4,16 @@
  basic GenerateText->GenerateText example
 */
 
-import {
-  Substrate,
-  GenerateText,
-  Graph,
-} from "@substratelabs/substrate-typescript";
+import { Substrate, GenerateText } from "@substratelabs/substrate-typescript";
 
 const SUBSTRATE_API_KEY = process.env["SUBSTRATE_API_KEY"];
 
 const substrate = new Substrate({
   apiKey: SUBSTRATE_API_KEY,
-  baseUrl: "https://api-staging.substrate.run",
 });
 const a = new GenerateText({
   prompt: "ask me a short trivia question in one sentence",
 }).output();
 const b = new GenerateText({ prompt: a.future.text }).output();
-const g = new Graph();
-g.add(a);
-g.add(b);
-const json = g.toJSON();
-// console.log(JSON.stringify(json, null, 2));
-const result = await substrate.compose(json);
+const result = await substrate.run(a, b);
 console.log(JSON.stringify(result, null, 2));
