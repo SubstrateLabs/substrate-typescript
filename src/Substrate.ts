22b8663973c7b46aeb7a022d1809c350817dcdef
Liam Griffiths <liamgriffiths@gmail.com>
2024-04-22 12:03:16 -0400

    Add `@throws` doc annotations for throwing functions (#65)
    * Add `@throws` doc annotations for throwing functions
    
    * Update docs a little more

--- src/Substrate.ts
+++ src/Substrate.ts
@@ -71,6 +71,10 @@ export class Substrate {
 
   /**
    *  Run the given nodes.
+   *
+   *  @throws {SubstrateError} when the server response is an error.
+   *  @throws {RequestTimeoutError} when the client has timed out (Configured by `Substrate.timeout`).
+   *  @throws {Error} when the client encounters an error making the request.
    */
   async run(...nodes: Node[]): Promise<SubstrateResponse> {
     const url = this.baseUrl + "/compose";
