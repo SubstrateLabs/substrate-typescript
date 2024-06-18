8a1ac87646a5f2ad1c9d2498f2ea6373fde2efe4
Liam Griffiths <liamgriffiths@gmail.com>
2024-06-18 10:59:01 -0400

    Include substrate-request-id in non-200 errors to help users diagnose (#86)
    * Include substrate-request-id in non-200 errors to help users diagnose
    
    * Format code

--- examples/implicit-nodes.ts
+++ examples/implicit-nodes.ts
@@ -8,15 +8,25 @@ async function main() {
   const substrate = new Substrate({ apiKey: SUBSTRATE_API_KEY });
 
   const a = new GenerateText(
-    { prompt: "tell me about windmills" },
+    { prompt: "tell me about windmills", max_tokens: 10 },
     { id: "a" },
   );
-  const b = new GenerateText({ prompt: a.future.text }, { id: "b" });
-  const c = new GenerateText({ prompt: b.future.text }, { id: "c" });
+  const b = new GenerateText(
+    { prompt: a.future.text, max_tokens: 10 },
+    { id: "b" },
+  );
+  const c = new GenerateText(
+    { prompt: b.future.text, max_tokens: 10 },
+    { id: "c" },
+  );
+  const d = new GenerateText(
+    { prompt: c.future.text, max_tokens: 10 },
+    { id: "d" },
+  );
 
   // Because the `c` is the the final node in the graph we can find nodes it depends
   // on through the relationships created via the input arguments.
-  const res = await substrate.run(c);
+  const res = await substrate.run(d);
   console.log(res.json);
 }
 main();
