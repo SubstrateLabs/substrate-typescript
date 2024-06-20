34b80f1e96d356561dfd4beaa22b093262ec1285
Liam Griffiths <liamgriffiths@gmail.com>
2024-06-20 13:02:03 -0400

    Updated version (#88)

--- bin/update-version.ts
+++ bin/update-version.ts
@@ -5,7 +5,7 @@ import { execSync } from "node:child_process";
 
 // NOTE: Merged with API version to produce the full SDK version string
 // https://docs.substrate.run/versioning
-const SDK_VERSION = "1.0.0";
+const SDK_VERSION = "1.0.1";
 
 const ok = (message: string) => console.log("\x1b[32m✓\x1b[0m", message);
 
