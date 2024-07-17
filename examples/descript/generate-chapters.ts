4d9e357d04f6696f7944e00167d2090165380fbb
Rob Cheung <kousun12@gmail.com>
2024-07-17 17:48:39 -0400

    --wip-- [skip ci]

--- examples/descript/generate-chapters.ts
+++ examples/descript/generate-chapters.ts
@@ -15,43 +15,48 @@ import {
  * https://media.substrate.run/kaufman-bafta-short.mp3
  * https://media.substrate.run/dfw-clip.m4a
  */
-// const audio_uri = "https://media.substrate.run/my-dinner-andre.m4a";
-const audio_uri = "https://media.substrate.run/federer-dartmouth.m4a";
-const substrate = new Substrate({
-  apiKey: process.env["SUBSTRATE_API_KEY"],
-});
-
-const outfile = process.argv[2] || "descript.html";
+// const sample = "https://media.substrate.run/my-dinner-andre.m4a";
+const sample = "https://media.substrate.run/federer-dartmouth.m4a";
+const substrate = new Substrate({ apiKey: process.env["SUBSTRATE_API_KEY"] });
+const audio_uri = process.argv[2] || sample;
+const outfile = process.argv[3] || "descript.html";
+const opts = { cache_age: 60 * 60 * 24 * 7 };
 async function main() {
   const transcribe = new TranscribeSpeech(
     { audio_uri, segment: true, align: true },
-    { cache_age: 60 * 60 * 24 * 7 },
+    opts,
   );
-  const chapters = new ComputeJSON({
-    prompt: sb.concat(
-      listChapters,
-      "\n\nTRANSCRIPT:\n\n",
-      transcribe.future.text,
-    ),
-    json_schema: proposedSchema,
-    model: "Mixtral8x7BInstruct",
-  });
-  const timestamps = new ComputeJSON({
-    prompt: sb.concat(
-      timestampPrompt,
-      "SECTIONS: ",
-      sb.jq<"string">(chapters.future.json_object, ".chapters | @json"),
-      "\n\nTRANSCRIPT:\n\n",
-      transcribe.future.text,
-    ),
-    json_schema: timestampedSchema,
-    model: "Mixtral8x7BInstruct",
-  });
+  const chapters = new ComputeJSON(
+    {
+      prompt: sb.concat(
+        listChapters,
+        "\n\nTRANSCRIPT:\n\n",
+        transcribe.future.text,
+      ),
+      json_schema: proposedSchema,
+      model: "Mixtral8x7BInstruct",
+    },
+    opts,
+  );
+  const timestamps = new ComputeJSON(
+    {
+      prompt: sb.concat(
+        timestampPrompt,
+        "SECTIONS: ",
+        sb.jq<"string">(chapters.future.json_object, ".chapters | @json"),
+        "\n\nTRANSCRIPT:\n\n",
+        transcribe.future.text,
+      ),
+      json_schema: timestampedSchema,
+      model: "Mixtral8x7BInstruct",
+    },
+    opts,
+  );
   const res = await substrate.run(transcribe, chapters, timestamps);
-  console.log(JSON.stringify(res.get(timestamps), null, 2));
   const transcript = res.get(transcribe);
   // @ts-ignore
   const timestampedChapters = res.get(timestamps).json_object?.chapters;
+
   const htmlTemplate = fs.readFileSync(`${currentDir}/index.html`, "utf8");
   const html = htmlTemplate
     .replace('"{{ segments }}"', JSON.stringify(transcript.segments, null, 2))
--- examples/descript/generate.ts
+++ examples/descript/generate.ts
@@ -9,10 +9,11 @@ import { currentDir } from "./util";
  * https://media.substrate.run/kaufman-bafta-short.mp3
  * https://media.substrate.run/dfw-clip.m4a
  */
-const audio_uri = "https://media.substrate.run/my-dinner-andre.m4a";
+const sample = "https://media.substrate.run/my-dinner-andre.m4a";
 const substrate = new Substrate({ apiKey: process.env["SUBSTRATE_API_KEY"] });
 
-const outfile = process.argv[2] || "descript.html";
+const audio_uri = process.argv[2] || sample;
+const outfile = process.argv[3] || "descript.html";
 async function main() {
   const transcribe = new TranscribeSpeech(
     { audio_uri, segment: true, align: true },
@@ -20,9 +21,10 @@ async function main() {
   );
   const res = await substrate.run(transcribe);
   const transcript = res.get(transcribe);
+
   const htmlTemplate = fs.readFileSync(`${currentDir}/index.html`, "utf8");
   const html = htmlTemplate
-    .replace('"{{ transcriptData }}"', JSON.stringify(transcript, null, 2))
+    .replace('"{{ segments }}"', JSON.stringify(transcript.segments, null, 2))
     .replace("{{ audioUrl }}", audio_uri);
   fs.writeFileSync(outfile, html);
 }
