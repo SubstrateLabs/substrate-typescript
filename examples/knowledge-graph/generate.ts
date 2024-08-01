7b45ad5e358724e0c51b67b02c7cbdb93ef30341
Rob Cheung <kousun12@gmail.com>
2024-08-01 15:48:23 -0400

    --wip-- [skip ci]

--- examples/knowledge-graph/generate.ts
+++ examples/knowledge-graph/generate.ts
@@ -7,26 +7,32 @@ const sampleBook = "War and Peace by Tolstoy";
 const book = process.argv[2] || sampleBook;
 const substrate = new Substrate({ apiKey: process.env["SUBSTRATE_API_KEY"] });
 
+const opts = { cache_age: 60 * 60 * 24 * 7 };
 async function main() {
-  const initialList = new ComputeText({
-    prompt: `List all the main characters in "${book}", then for all the meaningful relationships between them, provide a short label for the relationship`,
-    model: "Llama3Instruct405B",
-  });
-  const graph = new ComputeJSON({
-    prompt: sb.interpolate`Make a JSON graph composed of the characters in ${book} that illustrates the relationships between them. 
+  const initialList = new ComputeText(
+    {
+      prompt: `List all the main characters in "${book}", then for all the meaningful relationships between them, provide a short label for the relationship`,
+      model: "Llama3Instruct405B",
+    },
+    opts,
+  );
+  const graph = new ComputeJSON(
+    {
+      prompt: sb.interpolate`Make a JSON graph composed of the characters in ${book} that illustrates the relationships between them. 
 Use this context:
 ${initialList.future.text}`,
-    json_schema: jsonSchema,
-    temperature: 0.2,
-    model: "Llama3Instruct8B",
-  });
+      json_schema: jsonSchema,
+      temperature: 0.2,
+      model: "Llama3Instruct8B",
+    },
+    opts,
+  );
   const res = await substrate.run(graph);
   const jsonOut = res.get(graph).json_object;
   const htmlTemplate = fs.readFileSync(`${currentDir}/index.html`, "utf8");
-  const html = htmlTemplate.replace(
-    '"{{ graphData }}"',
-    JSON.stringify(jsonOut, null, 2),
-  );
+  const html = htmlTemplate
+    .replace('"{{ graphData }}"', JSON.stringify(jsonOut, null, 2))
+    .replaceAll("{{ title }}", book);
   fs.writeFileSync("knowledge-graph.html", html);
 }
 main();
