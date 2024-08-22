a0859fe93b87dd7794fd42cb384e83ab045edbb9
Liam Griffiths <liamgriffiths@gmail.com>
2024-08-22 16:03:16 -0400

    Update example

--- examples/jinja.ts
+++ examples/jinja.ts
@@ -5,10 +5,7 @@ import { Substrate, Box, sb } from "substrate";
 async function main() {
   const SUBSTRATE_API_KEY = process.env["SUBSTRATE_API_KEY"];
 
-  const substrate = new Substrate({
-    apiKey: SUBSTRATE_API_KEY,
-    baseUrl: "https://api-staging.substrate.run",
-  });
+  const substrate = new Substrate({ apiKey: SUBSTRATE_API_KEY });
 
   const a = new Box({ value: ["a", "b"] });
   const b = new Box({ value: { x: "x" } });
